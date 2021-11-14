b739071866c4c4f9e121c04464f9ba0a409622d5
melchj <31999176+melchj@users.noreply.github.com>
2021-11-13 22:04:26 -0500

    converted Fight.js from a hook function component thing to a react component class

--- client/src/components/Fight.js
+++ client/src/components/Fight.js
@@ -2,30 +2,36 @@ import React from 'react'
 import { getFight } from '../services/FightService'
 import './Fight.css'
 
-const Fight = () => {
+class Fight extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fightID: null,
+            fightData: null
+        };
+    }
 
-    let [fightID, setFightID] = React.useState('')
-    let [fightData, setFightData] = React.useState('')
-
-    const fetchData = (e) => {
-        getFight(fightID)
+    fetchData = (e) => {
+        getFight(this.state.fightID)
         .then((resp) => {
             // console.log(resp)
-            setFightData(resp)
+            // setFightData(resp)
+            this.setState({fightData: resp});
         })
         .catch((error) => {
             console.log(error)
         });
     }
 
-    const inputChange = (e) => {
+    inputChange = (e) => {
         // console.log(e.target.value)
-        setFightID(e.target.value);
+        // setFightID(e.target.value);
+        this.setState({fightID: e.target.value});
     }
 
-    const classStyle = (pos) => {
+    classStyle = (pos) => {
         const p = pos+"_class";
-        const cl = fightData && (" class-"+fightData[p]);
+        const cl = this.state.fightData && (" class-"+this.state.fightData[p]);
         // console.log(cl);
 
         var className = "class-icon me-3";
@@ -34,47 +40,49 @@ const Fight = () => {
         return className
     }
 
-    const numsOnly = (event) => {
+    numsOnly = (event) => {
         if (!/[0-9]/.test(event.key)) {
             event.preventDefault();
         }
     }
 
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="form-group">
-                    <input type="text" onKeyPress={(e) => numsOnly(e)} onChange={(e) => inputChange(e)} className="form-control" name="fightid" id="fightid" placeholder="Fight ID" />
-                    <button onClick={(e) => fetchData(e)} type='button' className="btn btn-primary">Load Fight ID</button>
-                </div>
-            </div>
-            <div className="card text-dark bg-light mt-3">
-                <div className="card-body">
-                    <div className="row">
-                        <h2 className="card-title">Fight #{fightData && fightData.fight_id}</h2>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-6">
-                            <h3>Winners</h3>
-                            <div className="row align-items-center"><div className={classStyle('w1')}></div>{fightData.w1_name}</div>
-                            <div className="row align-items-center"><div className={classStyle('w2')}></div>{fightData.w2_name}</div>
-                            <div className="row align-items-center"><div className={classStyle('w3')}></div>{fightData.w3_name}</div>
-                            <div className="row align-items-center"><div className={classStyle('w4')}></div>{fightData.w4_name}</div>
-                            <div className="row align-items-center"><div className={classStyle('w5')}></div>{fightData.w5_name}</div>
-                        </div>
-                        <div className="col-md-6">
-                            <h3>Losers</h3>
-                            <div className="row align-items-center"><div className={classStyle('l1')}></div>{fightData.l1_name}</div>
-                            <div className="row align-items-center"><div className={classStyle('l2')}></div>{fightData.l2_name}</div>
-                            <div className="row align-items-center"><div className={classStyle('l3')}></div>{fightData.l3_name}</div>
-                            <div className="row align-items-center"><div className={classStyle('l4')}></div>{fightData.l4_name}</div>
-                            <div className="row align-items-center"><div className={classStyle('l5')}></div>{fightData.l5_name}</div>
-                        </div>
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="form-group">
+                        <input type="text" onKeyPress={(e) => this.numsOnly(e)} onChange={(e) => this.inputChange(e)} className="form-control" name="fightid" id="fightid" placeholder="Fight ID" />
+                        <button onClick={(e) => this.fetchData(e)} type='button' className="btn btn-primary">Load Fight ID</button>
                     </div>
                 </div>
+                <div className="card text-dark bg-light mt-3">
+                    <div className="card-body">
+                        <div className="row">
+                            <h2 className="card-title">Fight #{this.state.fightData && this.state.fightData.fight_id}</h2>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <h3>Winners</h3>
+                                <div className="row align-items-center"><div className={this.classStyle('w1')}></div>{this.state.fightData && this.state.fightData.w1_name}</div>
+                                <div className="row align-items-center"><div className={this.classStyle('w2')}></div>{this.state.fightData && this.state.fightData.w2_name}</div>
+                                <div className="row align-items-center"><div className={this.classStyle('w3')}></div>{this.state.fightData && this.state.fightData.w3_name}</div>
+                                <div className="row align-items-center"><div className={this.classStyle('w4')}></div>{this.state.fightData && this.state.fightData.w4_name}</div>
+                                <div className="row align-items-center"><div className={this.classStyle('w5')}></div>{this.state.fightData && this.state.fightData.w5_name}</div>
+                            </div>
+                            <div className="col-md-6">
+                                <h3>Losers</h3>
+                                <div className="row align-items-center"><div className={this.classStyle('l1')}></div>{this.state.fightData && this.state.fightData.l1_name}</div>
+                                <div className="row align-items-center"><div className={this.classStyle('l2')}></div>{this.state.fightData && this.state.fightData.l2_name}</div>
+                                <div className="row align-items-center"><div className={this.classStyle('l3')}></div>{this.state.fightData && this.state.fightData.l3_name}</div>
+                                <div className="row align-items-center"><div className={this.classStyle('l4')}></div>{this.state.fightData && this.state.fightData.l4_name}</div>
+                                <div className="row align-items-center"><div className={this.classStyle('l5')}></div>{this.state.fightData && this.state.fightData.l5_name}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Fight
