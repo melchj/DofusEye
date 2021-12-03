8bb617497427761a95c41535cae15c980ae80177
melchj <31999176+melchj@users.noreply.github.com>
2021-12-02 21:37:46 -0500

    CLIENT: added bffs and rival stats to Stats component. functional but ugly!

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -37,124 +37,152 @@ class Stats extends React.Component {
                     <h2>Stats for {this.props.characterName}</h2>
                     <div className="row">
                         <div className='col'>
-                        <h3>All Fights:</h3>
-                        <table className="table table-hover table-sm w-50">
-                            <thead>
-                                <tr>
-                                    <th scope='col'></th>
-                                    <th scope='col'>5v5</th>
-                                    <th scope='col'>Total</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope='row'># Fights</th>
-                                    <td>{this.state.characterStats && this.state.characterStats['5v5Total']}</td>
-                                    <td>{this.state.characterStats && this.state.characterStats['AllTotal']}</td>
-                                </tr>
-                                <tr>
-                                    <th scope='row'>Wins</th>
-                                    <td>{this.state.characterStats && this.state.characterStats['5v5Wins']}</td>
-                                    <td>{this.state.characterStats && this.state.characterStats['AllWins']}</td>
-                                </tr>
-                                <tr>
-                                    <th scope='row'>Loses</th>
-                                    <td>{this.state.characterStats && (this.state.characterStats['5v5Total'] - this.state.characterStats['5v5Wins'])}</td>
-                                    <td>{this.state.characterStats && (this.state.characterStats['AllTotal'] - this.state.characterStats['AllWins'])}</td>
-                                </tr>
-                                <tr>
-                                    <th scope='row'>Winrate</th>
-                                    <td>{this.state.characterStats && (100 * this.state.characterStats['5v5Wins'] / this.state.characterStats['5v5Total']).toFixed(0)}%</td>
-                                    <td>{this.state.characterStats && (100 * this.state.characterStats['AllWins'] / this.state.characterStats['AllTotal']).toFixed(0)}%</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                            <h3>All Fights:</h3>
+                            <table className="table table-hover table-sm w-50">
+                                <thead>
+                                    <tr>
+                                        <th scope='col'></th>
+                                        <th scope='col'>5v5</th>
+                                        <th scope='col'>Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope='row'># Fights</th>
+                                        <td>{this.state.characterStats && this.state.characterStats['5v5Total']}</td>
+                                        <td>{this.state.characterStats && this.state.characterStats['AllTotal']}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope='row'>Wins</th>
+                                        <td>{this.state.characterStats && this.state.characterStats['5v5Wins']}</td>
+                                        <td>{this.state.characterStats && this.state.characterStats['AllWins']}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope='row'>Losses</th>
+                                        <td>{this.state.characterStats && (this.state.characterStats['5v5Total'] - this.state.characterStats['5v5Wins'])}</td>
+                                        <td>{this.state.characterStats && (this.state.characterStats['AllTotal'] - this.state.characterStats['AllWins'])}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope='row'>Winrate</th>
+                                        <td>{this.state.characterStats && (100 * this.state.characterStats['5v5Wins'] / this.state.characterStats['5v5Total']).toFixed(0)}%</td>
+                                        <td>{this.state.characterStats && (100 * this.state.characterStats['AllWins'] / this.state.characterStats['AllTotal']).toFixed(0)}%</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
 
                         <div className='col'>
-                        <h3>Attacks:</h3>
-                        <table className="table table-hover table-sm w-50">
-                            <thead>
-                                <tr>
-                                    <th scope='col'></th>
-                                    <th scope='col'>5v5</th>
-                                    <th scope='col'>Total</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope='row'># Fights</th>
-                                    <td>{this.state.characterStats && this.state.characterStats['5v5ATotal']}</td>
-                                    <td>{this.state.characterStats && this.state.characterStats['AllATotal']}</td>
-                                </tr>
-                                <tr>
-                                    <th scope='row'>Wins</th>
-                                    <td>{this.state.characterStats && this.state.characterStats['5v5AWins']}</td>
-                                    <td>{this.state.characterStats && this.state.characterStats['AllAWins']}</td>
-                                </tr>
-                                <tr>
-                                    <th scope='row'>Loses</th>
-                                    <td>{this.state.characterStats && (this.state.characterStats['5v5ATotal'] - this.state.characterStats['5v5AWins'])}</td>
-                                    <td>{this.state.characterStats && (this.state.characterStats['AllATotal'] - this.state.characterStats['AllAWins'])}</td>
-                                </tr>
-                                <tr>
-                                    <th scope='row'>Winrate</th>
-                                    <td>{this.state.characterStats && (100 * this.state.characterStats['5v5AWins'] / this.state.characterStats['5v5ATotal']).toFixed(0)}%</td>
-                                    <td>{this.state.characterStats && (100 * this.state.characterStats['AllAWins'] / this.state.characterStats['AllATotal']).toFixed(0)}%</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                            <h3>Attacks:</h3>
+                            <table className="table table-hover table-sm w-50">
+                                <thead>
+                                    <tr>
+                                        <th scope='col'></th>
+                                        <th scope='col'>5v5</th>
+                                        <th scope='col'>Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope='row'># Fights</th>
+                                        <td>{this.state.characterStats && this.state.characterStats['5v5ATotal']}</td>
+                                        <td>{this.state.characterStats && this.state.characterStats['AllATotal']}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope='row'>Wins</th>
+                                        <td>{this.state.characterStats && this.state.characterStats['5v5AWins']}</td>
+                                        <td>{this.state.characterStats && this.state.characterStats['AllAWins']}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope='row'>Losses</th>
+                                        <td>{this.state.characterStats && (this.state.characterStats['5v5ATotal'] - this.state.characterStats['5v5AWins'])}</td>
+                                        <td>{this.state.characterStats && (this.state.characterStats['AllATotal'] - this.state.characterStats['AllAWins'])}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope='row'>Winrate</th>
+                                        <td>{this.state.characterStats && (100 * this.state.characterStats['5v5AWins'] / this.state.characterStats['5v5ATotal']).toFixed(0)}%</td>
+                                        <td>{this.state.characterStats && (100 * this.state.characterStats['AllAWins'] / this.state.characterStats['AllATotal']).toFixed(0)}%</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
 
                         <div className='col'>
-                        <h3>Defences:</h3>
-                        <table className="table table-hover table-sm w-50">
-                            <thead>
-                                <tr>
-                                    <th scope='col'></th>
-                                    <th scope='col'>5v5</th>
-                                    <th scope='col'>Total</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope='row'># Fights</th>
-                                    <td>{this.state.characterStats && this.state.characterStats['5v5DTotal']}</td>
-                                    <td>{this.state.characterStats && this.state.characterStats['AllDTotal']}</td>
-                                </tr>
-                                <tr>
-                                    <th scope='row'>Wins</th>
-                                    <td>{this.state.characterStats && this.state.characterStats['5v5DWins']}</td>
-                                    <td>{this.state.characterStats && this.state.characterStats['AllDWins']}</td>
-                                </tr>
-                                <tr>
-                                    <th scope='row'>Loses</th>
-                                    <td>{this.state.characterStats && (this.state.characterStats['5v5DTotal'] - this.state.characterStats['5v5DWins'])}</td>
-                                    <td>{this.state.characterStats && (this.state.characterStats['AllDTotal'] - this.state.characterStats['AllDWins'])}</td>
-                                </tr>
-                                <tr>
-                                    <th scope='row'>Winrate</th>
-                                    <td>{this.state.characterStats && (100 * this.state.characterStats['5v5DWins'] / this.state.characterStats['5v5DTotal']).toFixed(0)}%</td>
-                                    <td>{this.state.characterStats && (100 * this.state.characterStats['AllDWins'] / this.state.characterStats['AllDTotal']).toFixed(0)}%</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                            <h3>Defenses:</h3>
+                            <table className="table table-hover table-sm w-50">
+                                <thead>
+                                    <tr>
+                                        <th scope='col'></th>
+                                        <th scope='col'>5v5</th>
+                                        <th scope='col'>Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope='row'># Fights</th>
+                                        <td>{this.state.characterStats && this.state.characterStats['5v5DTotal']}</td>
+                                        <td>{this.state.characterStats && this.state.characterStats['AllDTotal']}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope='row'>Wins</th>
+                                        <td>{this.state.characterStats && this.state.characterStats['5v5DWins']}</td>
+                                        <td>{this.state.characterStats && this.state.characterStats['AllDWins']}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope='row'>Losses</th>
+                                        <td>{this.state.characterStats && (this.state.characterStats['5v5DTotal'] - this.state.characterStats['5v5DWins'])}</td>
+                                        <td>{this.state.characterStats && (this.state.characterStats['AllDTotal'] - this.state.characterStats['AllDWins'])}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope='row'>Winrate</th>
+                                        <td>{this.state.characterStats && (100 * this.state.characterStats['5v5DWins'] / this.state.characterStats['5v5DTotal']).toFixed(0)}%</td>
+                                        <td>{this.state.characterStats && (100 * this.state.characterStats['AllDWins'] / this.state.characterStats['AllDTotal']).toFixed(0)}%</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
+
                     <div className='row'>
-                        <div className='col'>
+                        {/* <div className='col'>
                             <h3>Activity Chart:</h3>
                             <p>(insert here: a chart of the past month or two or something)</p>
+                        </div> */}
+                        <div className='col'>
+                            <h3>Best Friends:</h3>
+                            <p>(most common allies)</p>
+                            <ul>
+                                {this.state.characterStats && Object.keys(this.state.characterStats['most common allies']).map((key, i) => (
+                                    <li>{key}, {this.state.characterStats['most common allies'][key]}</li>
+                                ))}
+                            </ul>
                         </div>
                         <div className='col'>
-                            <h3>Nemesis:</h3>
-                            <p>(most common enemy)</p>
-                            <p>uhh idk, not implemented</p>
+                            <h3>Rivals:</h3>
+                            <p>(most common enemies)</p>
+                            <ul>
+                                {this.state.characterStats && Object.keys(this.state.characterStats['most common enemies']).map((key, i) => (
+                                    <li>{key}, {this.state.characterStats['most common enemies'][key]}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col'>
+                            <h3>Most Wins vs:</h3>
+                            <ul>
+                                {this.state.characterStats && Object.keys(this.state.characterStats['most often beat']).map((key, i) => (
+                                    <li>{key}, {this.state.characterStats['most often beat'][key]}</li>
+                                ))}
+                            </ul>
                         </div>
                         <div className='col'>
-                            <h3>Best Friend:</h3>
-                            <p>(most common teammate)</p>
-                            <p>hmmm... not implemented</p>
+                            <h3>Most Losses vs:</h3>
+                            <ul>
+                                {this.state.characterStats && Object.keys(this.state.characterStats['most often beaten by']).map((key, i) => (
+                                    <li>{key}, {this.state.characterStats['most often beaten by'][key]}</li>
+                                ))}
+                            </ul>
                         </div>
                     </div>
                 </div>
