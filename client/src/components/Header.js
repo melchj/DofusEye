dd2708a92f956fec4d1e8e5e445a462c41752953
melchj <31999176+melchj@users.noreply.github.com>
2021-12-31 09:02:56 -0500

    CLIENT: added links on homepage, fixed eye icon in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,7 +9,7 @@ export const Header = () => {
             <nav className='navbar navbar-expand-lg navbar-light bg-light'>
                 <div className='container-fluid'>
                     <Link to='/' className='navbar-brand'>
-                        <img src="../res/skull.png" alt="" width="30" height="24" class="d-inline-block align-text-top"/>
+                        <img src={require("../res/eye.png")} alt="" width="30" height="100%"/>
                         DofusEye
                     </Link>
                     <div className='navbar-nav'>
@@ -18,7 +18,6 @@ export const Header = () => {
                     </div>
                 </div>
             </nav>
-            this website is a super unfinished and experimental project, keep this in mind :D
         </div>
     );
 }
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
 
     return (
         <div>
-            this is the home page
+            <h1>Welcome to DofusEye!</h1>
+            <div>
+                This is a side project I've been working on for the Dofus guild Free Ringtones.
+                The goal is to collect data from Perceptor fight screenshots posted in the guild's Discord, then use this app to navigate through that data in a meaningful way.
+            </div>
+            <div>
+                This whole project is very experimental and mostly just for my own learning rather than to ship a finished product. We'll see what it becomes.
+            </div>
+            <div className='text-center'>
+                <h2> Start here: </h2>
+                <div className='m-2'>
+                    <Link to='/character'>
+                        <button className="btn btn-primary">Search for a character!!</button>
+                    </Link>
+                </div>
+                    <div className='m-2'>
+                        <Link to='/account'>
+                            <button className="btn btn-primary" disabled>Search for an account!!</button>
+                        </Link>
+                    <div>(the Account search is not yet implemented, come back soon!)</div>
+                </div>
+            </div>
         </div>
     );
 }
