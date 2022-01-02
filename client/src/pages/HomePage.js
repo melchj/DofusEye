d732eb5044709168a9d4adf2b1ccf5810b0ab220
melchj <31999176+melchj@users.noreply.github.com>
2022-01-02 15:45:08 -0500

    CLIENT: added text to home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -13,6 +13,7 @@ export default function HomePage() {
             <div>
                 This whole project is very experimental and mostly just for my own learning rather than to ship a finished product. We'll see what it becomes.
             </div>
+
             <div className='text-center'>
                 <h2> Start here: </h2>
                 <div className='m-2'>
@@ -26,6 +27,30 @@ export default function HomePage() {
                         </Link>
                     <div>(the Account search is not yet implemented, come back soon!)</div>
                 </div>
+            </div>
+
+            <h2>About the Data</h2>
+            <div>
+                The data currently comes entirely from screenshots posted on the Free Ringtones discord server. The screenshots are analyzed by a bot which reads names and classes and stores them in a database.
+            </div>
+            <div>
+                This method has some limitations:
+                <ul>
+                    <li>Screenshots are not always posted to the discord channel. People forget, or crash, or are not in Free Ringtones to begin with.</li>
+                    <li>The bot is not 100% accurate at reading names and classes. Sometimes it messes up and the data reflected on this app is inaccurate until the fight data is corrected.</li>
+                </ul>
+            </div>
+
+            <h2>Future Plans</h2>
+            <div>
+                This is a side project that I work on when I have time. Some potential future features are listed here. This list is not complete and does not show what features have priority.
+                <ul>
+                    <li>Making Discord Bot available to other guilds/servers, to get more data for this app outside of Free Ringtones.</li>
+                    <li>Leaderboards/Ladders</li>
+                    <li>A way to easily make corrections to screenshots that the bot misread. (to get the data more accurate).</li>
+                    <li>Stats for classes, accounts, guilds, etc...</li>
+                    <li>idk... more to come. message Chonk on discord with ideas/feedback/whatever.</li>
+                </ul>
             </div>
         </div>
     );
