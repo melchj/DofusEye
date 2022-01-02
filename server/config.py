6379e90263305136addc0a2d1cfc950a8b86e12b
melchj <31999176+melchj@users.noreply.github.com>
2022-01-01 21:51:33 -0500

    API: updated config.py database_url to work with heroku postgres quirk

--- server/config.py
+++ server/config.py
@@ -8,5 +8,7 @@ class Config:
     FLASK_APP = environ.get("FLASK_APP")
     FLASK_ENV = environ.get("FLASK_ENV")
     SECRET_KEY = environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1)
+    # replace statement abvoe is a workaround for a heroku postgres quirk
+    # see: https://stackoverflow.com/questions/66690321/flask-and-heroku-sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = False
