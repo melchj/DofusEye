fe78b42899c29984767c680185d776ee7a163bec
jack m <31999176+melchj@users.noreply.github.com>
2023-02-16 16:12:05 -0500

    fixing flask server for deployment in docker behind nginx

--- server/wgsi.py
+++ server/wgsi.py
@@ -1,3 +1,9 @@
 from app import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
+
+# https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
